f32dbb123941ccc2515da1f0a125f35e38a7a336
BrandNewXP <franc04alex@gmail.com>
2018-10-24 02:28:31 +0800

    Guess Number added.

--- ghw1/mainbot.py
+++ ghw1/mainbot.py
@@ -1,5 +1,6 @@
 import socket
 import random
+import time
 import datetime
 import urllib.request
 import urllib.parse
@@ -20,6 +21,7 @@ ZOutcome = ['Today is your lucky day!' ,
 			'You\'ve just wasted 3 seconds reading this message.' ,
 			'If you spell stressed backwards, you get desserts.' ,
 			'Wait, I\'m searching for some fortune messages on Google.'] #Total : 6
+GuessList = ['1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , '10']
 Youtube = '{}: https://www.youtube.com{}'
 YoutubePre = 'https://www.youtube.com/results?search_query={}'
 YoutubeCls = 'yt-uix-tile-link'
@@ -53,8 +55,12 @@ def IRCRobot():
 	sendmsg('JOIN {}'.format(Channel))
 	
 	#Auto introduction
-	sendmsg('PRIVMSG {} {}'.format(Channel , Intro))
+	sendmsg('PRIVMSG {} :{}'.format(Channel , Intro))
 
+	Guessing = False
+	answer = None
+	chances = None
+	
 	while True:
 		IRCMsg = IRCSocket.recv(Bufsize).decode().strip('\r\n')
 		if not IRCMsg :
@@ -63,7 +69,7 @@ def IRCRobot():
 		# PING PONG
 		if msg[0] == 'PING':
 			sendmsg('PONG ' + msg[1])
-			print('PINGed.')
+			print('PINGed at ' +  time.asctime(time.localtime(time.time())))
 			continue
 
 		ClientName , IRCCommand = msg[0].split('!', 1)[0][1:], msg[1]
@@ -72,16 +78,41 @@ def IRCRobot():
 		if IRCCommand == 'PRIVMSG':
 			action = msg[3][1:]
 			text = msg[4:]
-			if action in ZSign :
+			if Guessing == True :
+				if action in GuessList :
+					num = int(action)
+					if num == answer :
+						sendmsg('PRIVMSG {} :{}'.format(ClientName , 'Grats! You got it!'))
+						Guessing = False
+					elif num > answer :
+						sendmsg('PRIVMSG {} :{}'.format(ClientName , 'That\'s too big.'))
+						chances = chances - 1
+						sendmsg('PRIVMSG {} :{} {}'.format(ClientName , 'Chances left :' , chances))
+					else :
+						sendmsg('PRIVMSG {} :{}'.format(ClientName , 'Should be bigger.'))
+						chances = chances - 1
+						sendmsg('PRIVMSG {} :{} {}'.format(ClientName , 'Chances left :' , chances))
+				else :
+					sendmsg('PRIVMSG {} :{}'.format(ClientName , 'That\'s not valid before the holy guessing game ends!'))
+				if chances < 1 :
+					sendmsg('PRIVMSG {} :{}'.format(ClientName , 'Game over! Maybe next time.'))
+					Guessing = False
+			elif action in ZSign :
 				TodayDate = datetime.datetime.now()
 				Outcome = ZOutcome[(ZSign.index(action) * TodayDate.day) % 6]
-				sendmsg(Outcome)
+				sendmsg('PRIVMSG {} :{}'.format(ClientName , Outcome))
 			elif action == '!guess' :
-				sendmsg('Game start! Guess a number between 1~10.')
-				
+				if Guessing == True :
+					sendmsg('PRIVMSG {} :{}'.format(ClientName , 'Please finish previous game.'))
+					continue
+				sendmsg('PRIVMSG {} :{}'.format(ClientName , 'Game start! Guess a number between 1~10.'))
+				answer = random.randint(1 , 10)
+				print('Guessing game cheater :' , answer)
+				chances = 3
+				Guessing = True
 			elif action == '!song':
 				if not text :
-					sendmsg('I don\'t understand. You must have done something wrong.')
+					sendmsg('PRIVMSG {} :{}'.format(ClientName , 'I don\'t understand. You must have done something wrong.'))
 				else :
 					songname = '+'.join(text)
 					url = YoutubePre.format(urllib.parse.quote(songname))
@@ -91,10 +122,9 @@ def IRCRobot():
 						target = soup.find('a' , class_ = YoutubeCls)
 						title = target['title']
 						link =  target['href']
-						sendmsg(Youtube.format(title , link))
-						
+						sendmsg('PRIVMSG {} :{}'.format(ClientName , Youtube.format(title , link)))
 			else :
-				sendmsg('I don\'t understand. You must have misspelled something.')
+				sendmsg('PRIVMSG {} :{}'.format(ClientName , 'I don\'t understand. You must have misspelled something.'))
 			
 
 if __name__ == '__main__':
